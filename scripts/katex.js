dbb7d8531874e314e285371054f97ac9f60aa634
MaxCarroll0 <mjvcarroll@gmail.com>
2025-10-26 01:10:35 +0100

    fix display mode maths

--- scripts/katex.js
+++ scripts/katex.js
@@ -2,13 +2,10 @@ var katex = require("katex");
 var fs = require("fs");
 var input = fs.readFileSync(0);
 var displayMode = process.env.DISPLAY != undefined;
-console.log("KaTeX DISPLAY MODE:")
-if (displayMode) { console.log(displayMode) }
-else { console.log("null") }
 
 var html = katex.renderToString(String.raw`${input}`, {
     throwOnError : false,
-    displayModed : displayMode
+    displayMode : displayMode
 });
 
 console.log(html)
