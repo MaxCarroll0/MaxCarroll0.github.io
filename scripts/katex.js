7641d1aff8cd19213ebc4068f7474c5106313c1b
MaxCarroll0 <mjvcarroll@gmail.com>
2025-10-26 01:00:22 +0100

    Update katex.js

--- scripts/katex.js
+++ scripts/katex.js
@@ -2,6 +2,7 @@ var katex = require("katex");
 var fs = require("fs");
 var input = fs.readFileSync(0);
 var displayMode = process.env.DISPLAY != undefined;
+console.log("KaTeX DISPLAY MODE: " + process.enc.DISPLAY)
 
 var html = katex.renderToString(String.raw`${input}`, {
     throwOnError : false,
